50905597ee7cd149ac56aac41517debfd84dae4e
Vikrant Singh <vikrantsinghan5@gmail.com>
2026-02-16 14:12:57 +0530

    dg

--- src/app/(tabs)/_layout.jsx
+++ src/app/(tabs)/_layout.jsx
@@ -53,7 +53,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: "Home",
           tabBarIcon: ({ color, size, focused }) => (
